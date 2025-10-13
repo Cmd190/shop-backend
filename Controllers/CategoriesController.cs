412c6072f608143b6f04f97f7fe29ff9ef2cb6ab
Cmd190 <tonigaser@gmail.com>
2025-10-13 17:08:29 +0200

    added post and put for catgories

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -7,12 +7,48 @@ namespace Webshop.Controllers;
 [Route("[controller]")]
 public class CategoriesController(ILogger<CategoriesController> logger, ProductContext context) : ControllerBase
 {
-    private readonly ILogger<CategoriesController> _logger = logger;
+
+    //TODO introduce category dto
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<string>>> GetAllCategories()
+    public async Task<ActionResult<IEnumerable<Category>>> GetAllCategories()
     {
-        var categories = await context.Categories.Select(c => c.Name).ToListAsync();
+        var categories = await context.Categories.ToListAsync();
         return Ok(categories);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Category>> PostCategory(Category cat)
+    {
+          await context.Categories.AddAsync(cat);
+          await context.SaveChangesAsync();
+          return CreatedAtAction(cat.Name, new { id = cat.Id }, cat);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCategory(int id, Category category)
+    {
+        if (id != category.Id)
+        {
+            return BadRequest();
+        }
+
+        context.Entry(category).State = EntityState.Modified;
+
+        try
+        {
+            await context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException e)
+        {
+            if (! await context.Categories.AnyAsync(c => c.Id == id))
+            {
+                return NotFound();
+            }
+
+            logger.LogError($"Error while calling {nameof(PutCategory)} with {category}");
+        }
+
+        return NoContent();
     }
 }
